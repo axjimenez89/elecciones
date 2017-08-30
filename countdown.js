db711ce2d38a66132e3e24dcc11655c5b48cf81a
Alejandra Jimenez <axjimenez89@gmail.com>
2017-08-29 22:54:55 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,9 +1,4 @@
 
-// var now = moment();
-
-// var targetDay = now.format("2020-11-03", "dddd, MMMM Do YYYY");
-
-// var countDown= Math.floor(moment().diff(targetDay, 'seconds'));
 
 var now = moment();
 var targetDay = moment('2020-11-03', 'YYYY-MM-DD');
